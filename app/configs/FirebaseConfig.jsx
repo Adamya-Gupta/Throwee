9a49d6ae967323db0b754efd540c763f406bc20e
Adamya-Gupta <adamyagupta.2011@gmail.com>
2025-05-31 06:39:15 +0530

    firebase setup

---
+++ app/configs/FirebaseConfig.jsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "throwee-68f75.firebaseapp.com",
+  projectId: "throwee-68f75",
+  storageBucket: "throwee-68f75.firebasestorage.app",
+  messagingSenderId: "1051879305753",
+  appId: "1:1051879305753:web:90a25f5070fa893fa5b088",
+  measurementId: "G-C6DXDP0BXP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
--- app/provider.jsx
+++ app/provider.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import Header from './_components/Header'
+import { useUser } from '@clerk/nextjs';
+import axios from 'axios';
+import { UserDetailContext } from './_context/UserDetailContext';
 
 function Provider({ children }) {
 
+    const {user}=useUser();
+    const [userDetail,setUserDetail]=useState();
+
+    useEffect(() => {
+        user&&CheckUserAuth();
+    }, [user]);
+
     //Save user data
-    const CheckUserAuth=()=>{
+    const CheckUserAuth=async()=>{
     //Save User to Database
+    const result = await axios.post('/api/users',{
+        userName:user?.fullName,
+        userEmail:user?.primaryEmailAddress?.emailAddress
+    });
+    console.log(result.data);
+    setUserDetail(result.data);
     }
 
     return (
         <div>
+            <UserDetailContext.Provider value={{userDetail,setUserDetail}}>
             <Header />
             <div className='px-5 lg:px-5 xl:px-5 2xl:px-5'>
                 {children}
             </div>
+            </UserDetailContext.Provider>
         </div>
     )
 }
