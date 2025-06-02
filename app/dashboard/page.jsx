47f7e03b743ac572fdbaf6b309cc0bf464010e1b
Adamya-Gupta <adamyagupta.2011@gmail.com>
2025-06-02 23:38:21 +0530

    dashboard

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Info from './_componenets/info'
+import LogoList from './_componenets/LogoList'
+
+
+function Dashboard() {
+  return (
+    <div className='mt-20'>
+        <Info/>
+
+        <LogoList/>
+      
+    </div>
+  )
+}
+
+export default Dashboard
