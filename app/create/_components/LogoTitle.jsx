ba95dbc71f3110bb46a07b003f0cbe2165552edf
Adamya-Gupta <adamyagupta.2011@gmail.com>
2025-06-03 06:48:28 +0530

    removed usesearchparams

--- app/create/_components/LogoTitle.jsx
+++ app/create/_components/LogoTitle.jsx
@@ -6,9 +6,9 @@ import { useSearchParams } from 'next/navigation'
 
 
 function LogoTitle({onHandleInputChange,formData}) {
-    const searchParam=useSearchParams();
+    // const searchParam=useSearchParams();
     
-    const [title,setTitle]=useState(searchParam?.get('title')??'')
+    // const [title,setTitle]=useState(searchParam?.get('title')??'')
 
   return (
     <div className='my-10'>
